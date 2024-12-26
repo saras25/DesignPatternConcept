add303dfb9873b11d2c2293a9234dc246700de2b
saraswati gowala <saraswatigowala26@gmail.com>
2024-12-26 20:51:31 +0530

    Singleton design pattern

---
+++ SystemDesign/SystemDesign/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SystemDesign
+{
+    //Singleton design pattern
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Singleton singleton1 = Singleton.GetInstance();
+            Singleton singleton2 = Singleton.GetInstance();
+            Console.WriteLine(singleton1.GetHashCode());
+            Console.WriteLine(singleton2.GetHashCode());
+            //Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ SystemDesign/SystemDesign/Singleton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemDesign
+{
+    public class Singleton
+    {
+        //private instance variable
+        private static Singleton instance; 
+
+        //Private constructor
+        private Singleton()
+        {
+            
+        }
+
+        //static method
+        public static Singleton GetInstance()
+        {
+            if(instance == null)
+            {
+                //lazy initialization without using lock
+                instance = new Singleton();
+
+                //thread safe
+                lock (Singleton.instance)
+                {
+                    if(instance == null)
+                    {
+                        instance = new Singleton();
+                    }
+                }
+            }
+
+            return instance;
+
+        }
+
+
+
+    }
+}
